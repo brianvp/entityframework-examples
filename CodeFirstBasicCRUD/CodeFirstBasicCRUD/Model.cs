324ff69a7c220929f2f84746c2d5014285b573c3
Brian Vander Plaats <brian.vanderplaats@gmail.com>
2015-10-27 21:40:09 -0500

    Add CodeFirstBasicCRUD

---
+++ CodeFirstBasicCRUD/CodeFirstBasicCRUD/Model.cs
@@ -0,0 +1,60 @@
+namespace CodeFirstBasicCRUD
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("product.Model")]
+    public partial class Model
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Model()
+        {
+            PartNumbers = new HashSet<PartNumber>();
+        }
+
+        public int ModelId { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(25)]
+        public string ManufacturerCode { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public string Description { get; set; }
+
+        public string Features { get; set; }
+
+        public int? StatusId { get; set; }
+
+        public int? ManufacturerId { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal? ListPrice { get; set; }
+
+        public string ImageCollection { get; set; }
+
+        public string CategoryCustomData { get; set; }
+
+        public string ManufacturerCustomData { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? DateModified { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? DateCreated { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual Manufacturer Manufacturer { get; set; }
+
+        public virtual Status Status { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PartNumber> PartNumbers { get; set; }
+    }
+}
